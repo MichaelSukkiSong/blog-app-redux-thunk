c58b55918391802948a89eb431abd6c95dfa784b
sa77irak <sa77irak@gmail.com>
2021-01-01 23:33:04 +0900

    alternate overfetching solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,27 @@
-import jsonplaceholder from "../apis/jsonPlacehilder";
+import _ from "lodash";
+import jsonplaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  console.log("About to fetch posts");
+  await dispatch(fetchPosts());
+  console.log("fetched posts!");
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get("/posts");
 
-  dispatchEvent({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
